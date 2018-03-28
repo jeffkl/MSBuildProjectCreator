4a37ec503cda0a1bde2264754deed304ac5d3178
Jeff Kluge <jeffkl@microsoft.com>
2018-03-28 10:53:39 -0700

    Add ForEach method (#3)
    * Fix item order to be sequential
    * Add ForEach method
    * Add ForEach unit test
    * Switch unit tests to use single framework

---
+++ src/MSBuildProjectCreator/ProjectCreator.Linq.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Jeff Kluge. All rights reserved.
+//
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Build.Utilities.ProjectCreation
+{
+    public partial class ProjectCreator
+    {
+        /// <summary>
+        /// Executes the specified action for each item in a list.
+        /// </summary>
+        /// <typeparam name="T">The type of the item.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> containing items to iterate through.</param>
+        /// <param name="action">An <see cref="Action{T, ProjectCreator}"/> to execute against the current <see cref="ProjectCreator"/>.</param>
+        /// <returns>The current <see cref="ProjectCreator"/>.</returns>
+        public ProjectCreator ForEach<T>(IEnumerable<T> source, Action<T, ProjectCreator> action)
+        {
+            if (source != null)
+            {
+                foreach (T item in source)
+                {
+                    action(item, this);
+                }
+            }
+
+            return this;
+        }
+    }
+}
