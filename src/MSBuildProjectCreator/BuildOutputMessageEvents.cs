67401f48555752336c8e0726de2e08ea703ef9a4
Jeff Kluge <jeffkl@microsoft.com>
2018-05-22 15:28:36 -0700

    Improve BuildOutput to get message event Message as a list of strings (#11)
    Separate messages as strings and messages as events

---
+++ src/MSBuildProjectCreator/BuildOutputMessageEvents.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Jeff Kluge. All rights reserved.
+//
+// Licensed under the MIT license.
+
+using Microsoft.Build.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Build.Utilities.ProjectCreation
+{
+    /// <summary>
+    /// Represents the <see cref="BuildMessageEventArgs"/> that were logged during a build.
+    /// </summary>
+    public sealed class BuildOutputMessageEvents : IReadOnlyCollection<BuildMessageEventArgs>
+    {
+        private readonly IReadOnlyCollection<BuildMessageEventArgs> _messageEvents;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BuildOutputMessageEvents"/> class.
+        /// </summary>
+        /// <param name="messageEvents">A <see cref="IReadOnlyCollection{BuildMessageEventArgs}"/> containing the logged message events.</param>
+        internal BuildOutputMessageEvents(IReadOnlyCollection<BuildMessageEventArgs> messageEvents)
+        {
+            _messageEvents = messageEvents ?? throw new ArgumentNullException(nameof(messageEvents));
+        }
+
+        /// <inheritdoc cref="IReadOnlyCollection{T}.Count"/>
+        public int Count => _messageEvents.Count;
+
+        /// <summary>
+        /// Gets the <see cref="BuildMessageEventArgs"/> that were logged with <see cref="MessageImportance.High"/>.
+        /// </summary>
+        public IReadOnlyCollection<BuildMessageEventArgs> High => _messageEvents.Where(i => i.Importance == MessageImportance.High).ToList();
+
+        /// <summary>
+        /// Gets the <see cref="BuildMessageEventArgs"/> that were logged with <see cref="MessageImportance.Low"/>.
+        /// </summary>
+        public IReadOnlyCollection<BuildMessageEventArgs> Low => _messageEvents.Where(i => i.Importance == MessageImportance.Low).ToList();
+
+        /// <summary>
+        /// Gets the <see cref="BuildMessageEventArgs"/> that were logged with <see cref="MessageImportance.Normal"/>.
+        /// </summary>
+        public IReadOnlyCollection<BuildMessageEventArgs> Normal => _messageEvents.Where(i => i.Importance == MessageImportance.Normal).ToList();
+
+        /// <inheritdoc cref="IEnumerable{T}.GetEnumerator"/>
+        public IEnumerator<BuildMessageEventArgs> GetEnumerator()
+        {
+            return _messageEvents.GetEnumerator();
+        }
+
+        /// <inheritdoc cref="IEnumerable.GetEnumerator"/>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ src/MSBuildProjectCreator/BuildOutputMessages.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Jeff Kluge. All rights reserved.
+//
+// Licensed under the MIT license.
+
+using Microsoft.Build.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Build.Utilities.ProjectCreation
+{
+    /// <summary>
+    /// Represents the messages that were logged during a build.
+    /// </summary>
+    public sealed class BuildOutputMessages : IReadOnlyCollection<string>
+    {
+        private readonly BuildOutput _buildOutput;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BuildOutputMessages"/> class.
+        /// </summary>
+        /// <param name="buildOutput">The <see cref="BuildOutput"/> object that has message events.</param>
+        internal BuildOutputMessages(BuildOutput buildOutput)
+        {
+            _buildOutput = buildOutput ?? throw new ArgumentNullException(nameof(buildOutput));
+        }
+
+        /// <inheritdoc cref="IReadOnlyCollection{T}.Count"/>
+        public int Count => _buildOutput.MessageEvents.Count;
+
+        /// <summary>
+        /// Gets the messages that were logged with <see cref="MessageImportance.High"/>.
+        /// </summary>
+        public IReadOnlyCollection<string> High => _buildOutput.MessageEvents.High.Select(i => i.Message).ToList();
+
+        /// <summary>
+        /// Gets the messages that were logged with <see cref="MessageImportance.Low"/>.
+        /// </summary>
+        public IReadOnlyCollection<string> Low => _buildOutput.MessageEvents.Low.Select(i => i.Message).ToList();
+
+        /// <summary>
+        /// Gets the messages that were logged with <see cref="MessageImportance.Normal"/>.
+        /// </summary>
+        public IReadOnlyCollection<string> Normal => _buildOutput.MessageEvents.Normal.Select(i => i.Message).ToList();
+
+        /// <inheritdoc cref="IEnumerable{T}.GetEnumerator"/>
+        public IEnumerator<string> GetEnumerator()
+        {
+            return _buildOutput.MessageEvents.Select(i => i.Message).ToList().GetEnumerator();
+        }
+
+        /// <inheritdoc cref="IEnumerable.GetEnumerator"/>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
