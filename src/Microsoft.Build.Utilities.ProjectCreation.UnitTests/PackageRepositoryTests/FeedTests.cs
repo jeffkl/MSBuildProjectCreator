205c3705727d264c8b4f28ae63e2fad382d3f871
Jeff Kluge <jeffkl@microsoft.com>
2021-09-02 12:06:33 -0700

    Add option to add feeds to the package repository configuration
    Fixes #55

---
+++ src/Microsoft.Build.Utilities.ProjectCreation.UnitTests/PackageRepositoryTests/FeedTests.cs
@@ -0,0 +1,103 @@
+﻿// Copyright (c) Jeff Kluge. All rights reserved.
+//
+// Licensed under the MIT license.
+
+using NuGet.Configuration;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.Build.Utilities.ProjectCreation.UnitTests.PackageRepositoryTests
+{
+    public class FeedTests : TestBase
+    {
+        [Fact]
+        public void AddFeedPackageSource()
+        {
+            PackageSource packageSource = new PackageSource("https://custom.org/v3/index.json", "Custom")
+            {
+                IsEnabled = false,
+                ProtocolVersion = 7,
+            };
+
+            using PackageRepository packageRepository = PackageRepository.Create(TestRootPath)
+                .Feed(packageSource);
+
+            packageRepository.NuGetConfigPath.ShouldBe(Path.Combine(TestRootPath, "NuGet.Config"));
+
+            PackageSource actualPackageSource = EnumeratePackageSources(packageRepository).ShouldHaveSingleItem();
+
+            actualPackageSource.IsEnabled.ShouldBe(packageSource.IsEnabled);
+            actualPackageSource.IsOfficial.ShouldBe(packageSource.IsOfficial);
+            actualPackageSource.Name.ShouldBe(packageSource.Name);
+            actualPackageSource.ProtocolVersion.ShouldBe(packageSource.ProtocolVersion);
+            actualPackageSource.SourceUri.ShouldBe(packageSource.SourceUri);
+        }
+
+        [Fact]
+        public void AddFeedString()
+        {
+            const string uri = "https://custom.org/v3/index.json";
+
+            using PackageRepository packageRepository = PackageRepository.Create(TestRootPath)
+                .Feed(uri);
+
+            packageRepository.NuGetConfigPath.ShouldBe(Path.Combine(TestRootPath, "NuGet.Config"));
+
+            string actualPackageSourceUri = EnumeratePackageSourceUris(packageRepository).ShouldHaveSingleItem();
+
+            actualPackageSourceUri.ShouldBe(uri);
+        }
+
+        [Fact]
+        public void AddFeedUri()
+        {
+            Uri uri = new Uri("https://custom.org/v3/index.json");
+
+            using PackageRepository packageRepository = PackageRepository.Create(TestRootPath)
+                .Feed(uri);
+
+            packageRepository.NuGetConfigPath.ShouldBe(Path.Combine(TestRootPath, "NuGet.Config"));
+
+            PackageSource packageSource = EnumeratePackageSources(packageRepository).ShouldHaveSingleItem();
+
+            packageSource.SourceUri.ShouldBe(uri);
+        }
+
+        [Fact]
+        public void AddMultipleFeeds()
+        {
+            const string uri1 = "https://custom1.org/v3/index.json";
+            const string uri2 = "https://custom2.org/v3/index.json";
+
+            using PackageRepository packageRepository = PackageRepository.Create(TestRootPath)
+                .Feed(uri1)
+                .Feed(uri2);
+
+            packageRepository.NuGetConfigPath.ShouldBe(Path.Combine(TestRootPath, "NuGet.Config"));
+
+            EnumeratePackageSourceUris(packageRepository).ShouldBe(new[]
+            {
+                uri1,
+                uri2,
+            });
+        }
+
+        private IEnumerable<PackageSource> EnumeratePackageSources(PackageRepository packageRepository)
+        {
+            FileInfo nugetConfigFileInfo = new FileInfo(packageRepository.NuGetConfigPath);
+
+            ISettings settings = Settings.LoadDefaultSettings(nugetConfigFileInfo.DirectoryName, nugetConfigFileInfo.Name, new XPlatMachineWideSetting());
+
+            return PackageSourceProvider.LoadPackageSources(settings).ToList();
+        }
+
+        private IEnumerable<string> EnumeratePackageSourceUris(PackageRepository packageRepository)
+        {
+            return EnumeratePackageSources(packageRepository).Select(i => i.SourceUri.ToString());
+        }
+    }
+}
---
+++ src/Microsoft.Build.Utilities.ProjectCreation/PackageRepository.Feed.cs
@@ -0,0 +1,111 @@
+﻿// Copyright (c) Jeff Kluge. All rights reserved.
+//
+// Licensed under the MIT license.
+
+using NuGet.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Microsoft.Build.Utilities.ProjectCreation
+{
+    public partial class PackageRepository
+    {
+        private PackageRepository(string rootPath, IEnumerable<Uri> feeds = null)
+                    : this(rootPath)
+        {
+            Settings = new Settings(rootPath, NuGet.Configuration.Settings.DefaultSettingsFileName);
+
+            NuGetConfigPath = Settings.GetConfigFilePaths().First();
+
+            PackageSourceProvider = new PackageSourceProvider(Settings);
+
+            foreach (string packageSourceName in PackageSourceProvider.LoadPackageSources().Select(i => i.Name))
+            {
+                PackageSourceProvider.RemovePackageSource(packageSourceName);
+            }
+
+            SettingsUtility.SetConfigValue(Settings, ConfigurationConstants.GlobalPackagesFolder, GlobalPackagesFolder);
+
+            Settings.AddOrUpdate(ConfigurationConstants.PackageSources, new ClearItem());
+
+            if (feeds != null)
+            {
+                foreach (Uri feed in feeds)
+                {
+                    PackageSourceProvider.AddPackageSource(new PackageSource(feed.Host, feed.ToString()));
+                }
+            }
+
+            Settings.SaveToDisk();
+        }
+
+        /// <summary>
+        /// Gets the full path to the current NuGet.config.
+        /// </summary>
+        public string NuGetConfigPath { get; }
+
+        /// <summary>
+        /// Gets a <see cref="PackageSourceProvider" /> for the current NuGet configuration.
+        /// </summary>
+        public PackageSourceProvider PackageSourceProvider { get; }
+
+        /// <summary>
+        /// Gets the current NuGet configuration.
+        /// </summary>
+        public ISettings Settings { get; }
+
+        /// <summary>
+        /// Adds a feed to the current <see cref="PackageRepository" />.
+        /// </summary>
+        /// <param name="uri">The <see cref="Uri" /> of the feed.</param>
+        /// <param name="name">An optional name for the feed.</param>
+        /// <param name="additionalAttributes">Optional attributes for the feed.</param>
+        /// <returns>The current <see cref="PackageRepository" />.</returns>
+        public PackageRepository Feed(Uri uri, string name = default, IReadOnlyDictionary<string, string> additionalAttributes = default)
+        {
+            Feed(new PackageSource(uri.ToString(), name ?? uri.Host));
+
+            Settings.SaveToDisk();
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a feed to the current <see cref="PackageRepository" />.
+        /// </summary>
+        /// <param name="packageSource">A <see cref="PackageSource" /> object containing details about the feed.</param>
+        /// <returns>The current <see cref="PackageRepository" />.</returns>
+        public PackageRepository Feed(PackageSource packageSource)
+        {
+            PackageSourceProvider.AddPackageSource(packageSource);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a feed to the current <see cref="PackageRepository" />.
+        /// </summary>
+        /// <param name="directory">The <see cref="DirectoryInfo" /> of the feed.</param>
+        /// <param name="name">An optional name for the feed.</param>
+        /// <param name="additionalAttributes">Optional attributes for the feed.</param>
+        /// <returns>The current <see cref="PackageRepository" />.</returns>
+        public PackageRepository Feed(DirectoryInfo directory, string name = default, IReadOnlyDictionary<string, string> additionalAttributes = default)
+        {
+            return Feed(new Uri(directory.FullName), name, additionalAttributes);
+        }
+
+        /// <summary>
+        /// Adds a feed to the current <see cref="PackageRepository" />.
+        /// </summary>
+        /// <param name="uri">The URI of the feed.</param>
+        /// <param name="name">An optional name for the feed.</param>
+        /// <param name="additionalAttributes">Optional attributes for the feed.</param>
+        /// <returns>The current <see cref="PackageRepository" />.</returns>
+        public PackageRepository Feed(string uri, string name = default, IReadOnlyDictionary<string, string> additionalAttributes = default)
+        {
+            return Feed(new Uri(uri), name, additionalAttributes);
+        }
+    }
+}
