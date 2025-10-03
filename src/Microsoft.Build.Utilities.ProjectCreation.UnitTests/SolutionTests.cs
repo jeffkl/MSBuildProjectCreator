cd24a3cdfa09414520c7c9b5a0b4a50db777dc23
Jeff Kluge <jeffkl@microsoft.com>
2025-10-03 12:26:14 -0700

    WIP

---
+++ src/Microsoft.Build.Utilities.ProjectCreation.UnitTests/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Jeff Kluge. All rights reserved.
+//
+// Licensed under the MIT license.
+
+using System.IO;
+using Xunit;
+
+namespace Microsoft.Build.Utilities.ProjectCreation.UnitTests
+{
+    public class SolutionTests : TestBase
+    {
+        [Fact]
+        public void Test1()
+        {
+            ProjectCreator project1 = ProjectCreator.Templates.SdkCsproj()
+                .Save(Path.Combine(TestRootPath, "project1", "project1.csproj"));
+
+            SolutionCreator solution = SolutionCreator.Create(Path.Combine(TestRootPath, "solution1.sln"))
+                .Project(project1)
+                .Save();
+        }
+    }
+}
---
+++ src/Microsoft.Build.Utilities.ProjectCreation/SolutionCreator.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Jeff Kluge. All rights reserved.
+//
+// Licensed under the MIT license.
+
+using Microsoft.VisualStudio.SolutionPersistence;
+using Microsoft.VisualStudio.SolutionPersistence.Model;
+using Microsoft.VisualStudio.SolutionPersistence.Serializer;
+using System;
+using System.Threading;
+
+namespace Microsoft.Build.Utilities.ProjectCreation
+{
+    public partial class SolutionCreator
+    {
+        private SolutionFolderModel? _lastSolutionFolder = default;
+
+        private SolutionProjectModel? _lastProject = default;
+
+        private string _path;
+
+        private SolutionModel _solutionModel;
+
+        private SolutionCreator(string path)
+        {
+            _path = path;
+
+            _solutionModel = new SolutionModel();
+        }
+
+        public static SolutionCreator Create(string path)
+        {
+            return new SolutionCreator(path);
+        }
+
+        public SolutionCreator Folder(string path)
+        {
+            _lastSolutionFolder = _solutionModel.AddFolder(path);
+
+            return this;
+        }
+
+        public SolutionCreator Project(ProjectCreator project, string? projectTypeName = null)
+        {
+            _lastProject = _solutionModel.AddProject(project.FullPath, projectTypeName, folder: null);
+
+            return this;
+        }
+
+        public SolutionCreator Save()
+        {
+            ISolutionSerializer? serializer = SolutionSerializers.GetSerializerByMoniker(_path);
+
+            if (serializer == null)
+            {
+                throw new InvalidOperationException($"No solution serializer found for path '{_path}'.");
+            }
+
+            serializer.SaveAsync(_path, _solutionModel, CancellationToken.None).GetAwaiter().GetResult();
+
+            return this;
+        }
+    }
+}
